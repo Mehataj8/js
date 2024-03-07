eb02a2ca44a8cf85a4d5d331308d0831c88bbece
Mehataj8 <85029829+Mehataj8@users.noreply.github.com>
2024-03-07 19:52:37 +0530

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,21 @@
+let s1="hello";
+let s2="world";
+let result=s1 +" "+ s2;
+console.log(result);
+//
+let string = "hello world";
+let length = string.length;
+console.log(string.length);
+
+//
+let str = 'hello';
+str.charAt(2);
+//
+let a = 'hello';
+let b = 'all';
+let c = a + ' ' + b;
+console.log(c); 
+//
+let sentence = 'Hi all';
+console.log(sentence.toLowerCase());
+console.log(sentence.toUpperCase());
